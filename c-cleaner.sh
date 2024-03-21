0214cda03cb3466d87ebf5e30e4c36e592b61aee
webdragon63 <117004849+webdragon63@users.noreply.github.com>
2024-03-21 09:36:58 +0000

    Create c-cleaner.sh

---
+++ c-cleaner.sh
@@ -0,0 +1,12 @@
+# bin/bash
+echo CLEARING ENTIRE RAM
+echo INCREASING FPS
+echo REMOVING CACHES
+echo BOOSTING SPEED
+
+echo 1 > /proc/sys/vm/drop_caches
+echo 2 > /proc/sys/vm/drop_caches
+echo 3 > /proc/sys/vm/drop_caches
+
+echo ----------------------------PROCESS COMPLETED----------------------------------
+echo ---------- THANKS FOR USING OUR SOFTWARE. ALL CACHES ARE REMOVED. -------------
