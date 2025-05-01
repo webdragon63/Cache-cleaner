257fc70c7b93fcddc35f05af1af9ec0cb35bb8d7
WebDragon <117004849+webdragon63@users.noreply.github.com>
2025-05-01 16:41:26 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sleep 1
+VERMILION='\033[0;31m'
+if [[ $(id -u) -ne 0 ]] ; then 
+echo -e "${VERMILION}You are Not Root! Please Run as Root."
+echo -e "${VERMILION}Installation failed ..." ; exit 1 ; 
+fi
+VERMILION='\033[0;31m'
+WHITE='\033[0;37m'
+GREEN='\033[0;32m'
+
+echo -e "${VERMILION}Installing ..."
+cp -r c-cleaner.sh /usr/local/bin/
+cp -r c-cleaner.sh /usr/bin/
+ln -sf /usr/local/bin/c-cleaner.sh /usr/local/bin/clean
+ln -sf /usr/bin/c-cleaner.sh /usr/bin/clean
+chmod 777 /usr/local/bin/c-cleaner.sh
+chmod 777 /usr/bin/c-cleaner.sh
+sleep 1
+echo -e "${WHITE}Tool has been installed successfully ..."
+echo -e "${GREEN}Run ${WHITE}clean ${GREEN}on your root terminal to clean your cache."
