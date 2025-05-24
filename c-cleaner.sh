c77a69eec32eac13bc75bedad8d2a20dc91abca7
WebDragon <117004849+webdragon63@users.noreply.github.com>
2025-05-24 17:23:48 +0000

    Update c-cleaner.sh

--- c-cleaner.sh
+++ c-cleaner.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-CYAN="\e[96m"
+
+# Define ANSI color codes
+CYAN='\e[96m'
+RESET='\e[0m'
+ITALIC='\e[3m'
+BOLD='\e[1m'
 if [[ $(id -u) -ne 0 ]] ; then 
    echo -e "${CYAN}You are Not Root! Please Run as root" ; exit 1 ; 
 fi
-CYAN="\e[96m"
 
-echo -e  "${CYAN}"
-echo CLEARING ENTIRE RAM
-echo INCREASING FPS
-echo REMOVING CACHES
-echo BOOSTING SPEED
-
+# Cyberpunk Styled Output
+echo -e "${CYAN}${ITALIC}${BOLD}CLEARING ENTIRE RAM${RESET}"
+echo -e "${CYAN}${ITALIC}${BOLD}INCREASING FPS${RESET}"
+echo -e "${CYAN}${ITALIC}${BOLD}REMOVING CACHES${RESET}"
+echo -e "${CYAN}${ITALIC}${BOLD}BOOSTING SPEED${RESET}"
+echo ""
 echo 1 > /proc/sys/vm/drop_caches
 echo 2 > /proc/sys/vm/drop_caches
 echo 3 > /proc/sys/vm/drop_caches
-
-echo ----------------------------PROCESS COMPLETED----------------------------------
-echo ---------- THANKS FOR USING OUR SOFTWARE. ALL CACHES ARE REMOVED. -------------
+echo -e "${BOLD}-----------------------------${CYAN}PROCESS COMPLETED${RESET}${BOLD}---------------------------------${RESET}"
+echo -e "${BOLD}-----------${ITALIC}${BOLD}${CYAN}THANKS FOR USING OUR SOFTWARE. ALL CACHES ARE REMOVED.${RESET}${BOLD}--------------${RESET}"
